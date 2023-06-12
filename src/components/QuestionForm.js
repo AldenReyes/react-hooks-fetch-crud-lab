5b4bb67e36f95550cd433214678a8c63847b7c4a
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2023-06-12 02:32:05 -0700

    Adds questions to page by state using fetch

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -19,7 +19,24 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    const newQuestion = {
+      prompt: formData.prompt,
+      answers: [
+        formData.answer1,
+        formData.answer2,
+        formData.answer3,
+        formData.answer4,
+      ],
+      correctIndex: formData.correctIndex,
+    };
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+
+      body: JSON.stringify(newQuestion),
+    })
+      .then((res) => res.json())
+      .then((formData) => console.log(formData));
   }
 
   return (
@@ -84,7 +101,9 @@ function QuestionForm(props) {
             <option value="3">{formData.answer4}</option>
           </select>
         </label>
-        <button type="submit">Add Question</button>
+        <button type="submit" onSubmit={handleSubmit}>
+          Add Question
+        </button>
       </form>
     </section>
   );
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((res) => res.json())
+      .then((questionsData) => setQuestions(questionsData));
+  }, []);
+
+  // const handleAddQuestion(question) {
+  //   setQuestions(...questions, question)
+  // }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((question) => {
+          return <QuestionItem key={question.id} question={question} />;
+        })}
+      </ul>
     </section>
   );
 }
